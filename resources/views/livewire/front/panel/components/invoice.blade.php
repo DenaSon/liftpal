d8e68c6d3345c958c55c3b95f78e7425b1849112
milad <asadpour.milad@gmail.com>
2024-06-30 21:55:17 +0330

    invoice-page

--- resources/views/livewire/front/panel/components/invoice.blade.php
+++ resources/views/livewire/front/panel/components/invoice.blade.php
@@ -1,3 +1,64 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-</div>
+<div class="contaner">
+
+
+    <div class="m-auto row"><span class="h5 text-center bg-secondary p-2 shadow-sm border rounded border-tob-primary">جزئیات سفارش </span>
+    </div>
+
+    <div class="row m-auto bg-secondary p-2 shadow-sm border rounded border-tob-primary"><span class="fs-4 text-end ">اطلاعات سفارش </span>
+        <hr dir="rtl" class=" custom-hr-invoice mt-2">
+        <div class="row col"><h6><span class="d-block fs-5 my-2">شماره سفارش</span> <span class="d-block">108</span></h6></div>
+        <div class="row col"><h6><span class="d-block fs-5 my-2">تاریخ سفارش</span> <span class="d-block">108</span></h6></div>
+        <div class="row col"><h6><span class="d-block fs-5 my-2">مبلغ کل</span> <span class="d-block">108</span></h6></div>
+        <div class="row col"><span class="d-block my-2">مبلغ کل</span> <span class="d-block">108</span></div>
+    </div>
+
+
+    <div class="container mt-3">
+        <div class="row g-3 d-flex align-items-center justify-content-md-around">
+            <div class="col me-1 bg-secondary p-2 shadow-sm border rounded">
+                <div class="row">
+                    <span class="h5 text-end p-2">جزئیات سفارش</span>
+                </div>
+                <hr dir="rtl" class="custom-hr-invoice mt-1">
+                <div class="row">
+                    <h6 class="d-flex justify-content-between my-3">
+                        <span>تعداد کل</span><span class="text-end">108</span>
+                    </h6>
+                    <h6 class="d-flex justify-content-between my-3">
+                        <span>زمان سفارش</span><span class="text-end">108</span>
+                    </h6>
+                    <h6 class="d-flex justify-content-between my-3">
+                        <span>زمان تقریبی تحویل</span><span class="text-end">108</span>
+                    </h6>
+                </div>
+            </div>
+
+            <div class="col ms-1 bg-secondary p-2 shadow-sm border rounded">
+                <div class="row">
+                    <span class="h5 text-end p-2">جزئیات پرداخت</span>
+                </div>
+                <hr dir="rtl" class="custom-hr-invoice mt-2">
+                <div class="row">
+                    <h6 class="d-flex justify-content-between my-3">
+                        <span>مالیات</span><span class="text-end">108</span>
+                    </h6>
+                    <h6 class="d-flex justify-content-between my-3">
+                        <span>هزینه ارسال</span><span class="text-end">108</span>
+                    </h6>
+                    <h6 class="d-flex justify-content-between my-3">
+                        <span>تخفیف</span><span class="text-end">108</span>
+                    </h6>
+                </div>
+            </div>
+
+    </div>
+
+    </div>
+
+        <div class="container mx-auto row mt-2 bg-secondary p-2 shadow-sm border rounded "><span class=" text-end ">ادرس دریافت مرسوله </span>
+        <hr dir="rtl" class=" custom-hr-invoice mt-2">
+        <div class="row col"><h6><span class="d-block fs-5 my-2">شماره سفارش</span> <span class="d-block">108</span></h6></div>
+
+     </div>
+
+    </div>
