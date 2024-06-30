1ba23072c77bea0fa190403cc2471812a457b720
milad <asadpour.milad@gmail.com>
2024-06-30 14:50:14 +0330

    initial complete of panel

---
+++ resources/views/livewire/front/panel/components/main.blade.php
@@ -0,0 +1,40 @@
+<div>
+
+    <div class="container mx-auto text-center row border d-flex rounded-2 p-3 border-info mt-5 mb-5 ps-2 justify-content-center align-items-center">
+
+        <div class="col flex-col justify-content-between "><span class=" mb-5 ">&nbsp;&nbsp;سفارش</span><img class="mt-2" src="{{ asset('assets/img/client-panel/status-delivered.svg') }}" alt=""></div>
+        <div class="col"><span class="me-2">سفارش جاری</span ><img class="mt-2" src="{{ asset('assets/img/client-panel/status-processing.svg') }}" alt=""></div>
+        <div class="col"><span  "> سفارش مرجوعی</span><img class="mt-2" src="{{ asset('assets/img/client-panel/status-returned.svg') }}" alt=""></div>
+
+
+    </div>
+
+<div>
+    <table class="contianer table mt-5 ">
+        <thead class="fw-bold">
+        <tr>
+            <th colspan="5">شماره سفارش</th>
+            <th colspan="1" class="text-center">وضعیت</th>
+        </tr>
+        </thead>
+        <tbody class="table-group-divider fw-bold bold-divider">
+        <tr>
+            <td colspan="5">1</td>
+            <td colspan="1" class="text-center">پرداخت شده</td>
+        </tr>
+        <tr>
+            <td colspan="5">2</td>
+            <td colspan="1" class="text-center">مرجوعی</td>
+        </tr>
+        <tr>
+            <td colspan="5">3</td>
+            <td colspan="1" class="text-center">در حال بررسی</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
+
+
+
+</div>
