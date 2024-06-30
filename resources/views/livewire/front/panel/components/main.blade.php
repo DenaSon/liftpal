d5ad4411d24062dbfe6a440fc43bb38365a90a65
Mohammad Asadi <Mohammad.wartan@gmail.com>
2024-06-30 19:11:59 +0330

    pretty changes for main file in clientarea

--- resources/views/livewire/front/panel/components/main.blade.php
+++ resources/views/livewire/front/panel/components/main.blade.php
@@ -1,11 +1,27 @@
 <div>
 
-    <div class="container mx-auto text-center row border d-flex rounded-bottom p-3  mt-5 mb-2 ps-2 justify-content-center align-items-center" style="border:1px solid rgba(251,86,49,0.42)
+    <div id="paginated-orders" class="container mx-auto text-center row border d-flex rounded-bottom p-3  mt-5 mb-2 ps-2 justify-content-center align-items-center" style="border:1px solid rgba(225,155,139,0.34)
     !important;">
 
-        <div class="col flex-col justify-content-between "><span class=" mb-5 ">&nbsp;&nbsp;سفارش</span><img class="mt-2" src="{{ asset('assets/img/client-panel/status-delivered.svg') }}" alt=""></div>
-        <div class="col"><span class="me-2">سفارش جاری</span ><img class="mt-2" src="{{ asset('assets/img/client-panel/status-processing.svg') }}" alt=""></div>
-        <div class="col"><span> سفارش مرجوعی</span><img class="mt-2" src="{{ asset('assets/img/client-panel/status-returned.svg') }}" alt=""></div>
+        <div class="col flex-col justify-content-between ">
+            <button class="btn btn-outline-info">
+                <i class="fi fi-truck me-2"></i>
+                <span>ارسال شده</span>
+            </button>
+        </div>
+        <div class="col flex-col justify-content-between ">
+
+            <button class="btn btn-outline-success">
+                <i class="fi fi-check me-2"></i>
+                دریافت شده
+            </button>
+        </div>
+        <div class="col flex-col justify-content-between ">
+            <button class="btn btn-outline-warning">
+                <i class="fi-logout  me-2 "></i>
+                مرجوعی
+            </button>
+        </div>
 
 
     </div>
@@ -13,55 +29,63 @@
 
     <div class="table-responsive">
         <table class="container table mt-4 table-hover">
-        <thead class="fw-bold" style="border-bottom: 2px solid #807878">
-        <tr>
-            <th colspan="3">شماره سفارش</th>
-            <th colspan="1" class="text-center">وضعیت</th>
-            <th colspan="1" class="text-center">مبلغ
-                <span class="fs-xs muted fw-normal">(تومان)</span>
-            <th colspan="1" class="text-center">زمان</th>
-            </th>
-        </tr>
-        </thead>
-        <tbody class="table-group-divider fw-bold bold-divider">
-        @foreach($orders as $order)
+            <thead class="fw-bold" style="border-bottom: 2px solid #807878">
+            <tr>
+                <th colspan="3">شماره سفارش</th>
+                <th colspan="1" class="text-center">وضعیت</th>
+                <th colspan="1" class="text-center">مبلغ
+                    <span class="fs-xs muted fw-normal">(تومان)</span>
+                <th colspan="1" class="text-center">زمان</th>
+                </th>
+            </tr>
+            </thead>
+            <tbody class="table-group-divider fw-bold bold-divider">
+            @foreach($orders as $order)
+
+                <tr>
+                    <td colspan="3">{{ $order->order_number }}</td>
+                    <td colspan="1" class="text-center">
+                        @switch($order->status)
+                            @case('received')
+                                <span class="badge text-bg-secondary text-black">بررسی</span>
+                                @break
+                            @case('process')
+                                <span class="badge text-bg-primary text-white">پردازش</span>
+                                @break
+                            @case('shipped')
+                                <span class="badge text-bg-info text-white">ارسال شده</span>
+                                @break
+                            @case('delivered')
+                                <span class="badge text-bg-success text-white">دریافت شده</span>
+                                @break
+                            @case('return')
+                                <span class="badge text-bg-warning text-white"> مرجوعی</span>
+                                @break
+
+                        @endswitch
 
 
-        <tr>
-            <td colspan="3">{{ $order->order_number }}</td>
-            <td colspan="1" class="text-center">
-                @switch($order->status)
-                    @case('received')
-                        <span class="badge text-bg-secondary text-black">بررسی</span>
-                        @break
-                    @case('process')
-                        <span class="badge text-bg-warning text-white">پردازش</span>
-                        @break
-                    @case('shipped')
-                        <span class="badge text-bg-info text-white">ارسال شده</span>
-                        @break
-                    @case('delivered')
-                        <span class="badge text-bg-success text-white">دریافت شده</span>
-                        @break
+                    </td>
+                    <td colspan="1" class="text-center">{{ number_format($order->total_price ) }}</td>
 
-                @endswitch
+                    <td colspan="1" class="text-center fw-normal fs-xs">{{ jdate($order->created_at)->format('d M Y')  }}</td>
+                </tr>
 
-
-            </td>
-            <td colspan="1" class="text-center">{{ number_format($order->total_price ) }}</td>
-
-            <td colspan="1" class="text-center fw-normal fs-xs">{{ jdate($order->created_at)->format('d M Y')  }}</td>
-        </tr>
-
-        @endforeach
-        </tbody>
-    </table>
+            @endforeach
+            <div class="mt-4 text-center">
+            <span wire:loading.delay class="text-info fw-bold fs-sm">
+            درحال دریافت...
+            </span>
+            </div>
+            </tbody>
+        </table>
     </div>
 
     <div class="d-flex justify-content-center mt-4">
-        {{ $orders->links() }}
-    </div>
+        {{ $orders->links(data: ['scrollTo' => '#paginated-orders']) }}
 
+
+    </div>
 
 
 </div>
