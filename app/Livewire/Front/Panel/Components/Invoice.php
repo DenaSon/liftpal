e2668d639699cf898dcb376529fdbdd0b755e8d4
Mohammad Asadi <Mohammad.wartan@gmail.com>
2024-07-01 10:26:35 +0330

    changes on clientarea 01072024.

--- app/Livewire/Front/Panel/Components/Invoice.php
+++ app/Livewire/Front/Panel/Components/Invoice.php
@@ -2,10 +2,30 @@
 
 namespace App\Livewire\Front\Panel\Components;
 
+use App\Models\Order;
 use Livewire\Component;
 
 class Invoice extends Component
 {
+    public $order = [];
+
+    public function mount()
+    {
+        $authUserId = auth()->user()->id;
+        $orderId = request()->input('order');
+
+        if (!is_numeric($orderId)) {
+            abort(400, 'Invalid order ID');
+        }
+
+
+        $this->order = Order::whereUserId($authUserId)->whereOrderNumber($orderId)->first();
+
+        if (!$this->order) {
+            abort(404);
+        }
+
+    }
     public function render()
     {
         return view('livewire.front.panel.components.invoice');
--- resources/views/livewire/front/panel/components/invoice.blade.php
+++ resources/views/livewire/front/panel/components/invoice.blade.php
@@ -1,64 +1,89 @@
-<div class="contaner">
-
-
-    <div class="m-auto row"><span class="h5 text-center bg-secondary p-2 shadow-sm border rounded border-tob-primary">جزئیات سفارش </span>
-    </div>
-
-    <div class="row m-auto bg-secondary p-2 shadow-sm border rounded border-tob-primary"><span class="fs-4 text-end ">اطلاعات سفارش </span>
-        <hr dir="rtl" class=" custom-hr-invoice mt-2">
-        <div class="row col"><h6><span class="d-block fs-5 my-2">شماره سفارش</span> <span class="d-block">108</span></h6></div>
-        <div class="row col"><h6><span class="d-block fs-5 my-2">تاریخ سفارش</span> <span class="d-block">108</span></h6></div>
-        <div class="row col"><h6><span class="d-block fs-5 my-2">مبلغ کل</span> <span class="d-block">108</span></h6></div>
-        <div class="row col"><span class="d-block my-2">مبلغ کل</span> <span class="d-block">108</span></div>
-    </div>
-
-
-    <div class="container mt-3">
-        <div class="row g-3 d-flex align-items-center justify-content-md-around">
-            <div class="col me-1 bg-secondary p-2 shadow-sm border rounded">
-                <div class="row">
-                    <span class="h5 text-end p-2">جزئیات سفارش</span>
+<div>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card shadow-lg">
+                <div class="card-header text-center text-primary">
+                    <span class="fw-bold">اطلاعات سفارش</span>
                 </div>
-                <hr dir="rtl" class="custom-hr-invoice ">
-                <div class="row">
-                    <h6 class="d-flex justify-content-between my-3">
-                        <span>تعداد کل</span><span class="text-end">108</span>
-                    </h6>
-                    <h6 class="d-flex justify-content-between my-3">
-                        <span>زمان سفارش</span><span class="text-end">108</span>
-                    </h6>
-                    <h6 class="d-flex justify-content-between my-3">
-                        <span>زمان تقریبی تحویل</span><span class="text-end">108</span>
-                    </h6>
-                </div>
-            </div>
+                <div class="card-body border-2">
 
-            <div class="col ms-1 bg-secondary p-2 shadow-sm border rounded">
-                <div class="row">
-                    <span class="h5 text-end p-2">جزئیات پرداخت</span>
-                </div>
-                <hr dir="rtl" class="custom-hr-invoice">
-                <div class="row">
-                    <h6 class="d-flex justify-content-between my-3">
-                        <span>مالیات</span><span class="text-end">108</span>
-                    </h6>
-                    <h6 class="d-flex justify-content-between my-3">
-                        <span>هزینه ارسال</span><span class="text-end">108</span>
-                    </h6>
-                    <h6 class="d-flex justify-content-between my-3">
-                        <span>تخفیف</span><span class="text-end">108</span>
-                    </h6>
+                <ul class="list-group border-0">
+                        <li class="list-group-item d-flex justify-content-between align-items-center border-0 border-bottom">
+                            <span>
+
+                                شماره سفارش
+                            </span>
+                            <span class="">{{ $order?->order_number }}</span>
+                        </li>
+
+
+                        <li class="list-group-item d-flex justify-content-between align-items-center border-0 border-bottom">
+                            <span>
+
+                               مبلغ سفارش
+                            </span>
+                            <span class="">{{ number_format($order->total_price) }}
+                            <span class="text-muted fs-xs"> تومان</span>
+                            </span>
+                        </li>
+
+                        <li class="list-group-item d-flex justify-content-between align-items-center border-0">
+                            <span>
+
+                             تاریخ
+                            </span>
+                            <span class="">{{ jdate($order->created_at)->toDateString() }}
+
+                            </span>
+                        </li>
+
+                    </ul>
                 </div>
             </div>
+        </div>
+
+        <div class="col-sm-6">
+            <div class="card shadow-lg">
+                <div class="card-header text-center text-primary">
+                    <span class="fw-bold">جزئیات سفارش</span>
+                </div>
+                <div class="card-body border-2">
+
+                    <ul class="list-group border-0">
+                        <li class="list-group-item d-flex justify-content-between align-items-center border-0 border-bottom">
+                            <span>
+
+                               تعداد
+                            </span>
+                            <span class="">10 محصول</span>
+                        </li>
+
+
+                        <li class="list-group-item d-flex justify-content-between align-items-center border-0 border-bottom">
+                            <span>
+
+                              ساعت ثبت
+                            </span>
+                            <span class="">9:25
+                            <span class="text-muted fs-xs"> تومان</span>
+                            </span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center border-0 border-bottom">
+                            <span>
+
+                             زمان تحویل
+                            </span>
+
+                            <span class="fs-xs">  1 خرداد تا 5 فروردین</span>
+
+                        </li>
+
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
 
     </div>
-
-    </div>
-
-        <div class="container mx-auto row mt-2 bg-secondary p-2 shadow-sm border rounded "><span class=" text-end ">ادرس دریافت مرسوله </span>
-        <hr dir="rtl" class=" custom-hr-invoice mt-2">
-        <div class="row col"><h6><span class="d-block fs-5 my-2">شماره سفارش</span> </h6></div>
-
-     </div>
-
-    </div>
+</div>
