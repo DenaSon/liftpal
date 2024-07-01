25e3cd685361218f59dfba2da9c4e6d1dc667a2e
milad <asadpour.milad@gmail.com>
2024-07-01 10:35:28 +0330

    invoice-page

--- resources/views/livewire/front/panel/clientarea.blade.php
+++ resources/views/livewire/front/panel/clientarea.blade.php
@@ -21,7 +21,7 @@
             <!-- Content-->
             <div class="col-lg-8 col-md-7 mb-5 account">
                 <h1 class="fw-normal fs-5">{{ $pageTitle ?? 'اطلاعات حساب کاربری' }}</h1>
-
+                <hr dir="rtl" class=" custom-hr-title-panel mt-2" >
                 @if(request()->input('page') == 'profile')
                     <livewire:front.panel.components.profile />
                 @elseif(request()->input('page') == 'main')
