0f4f5dc3f819055466d67558ead560725bd3b194
Mohammad Asadi <Mohammad.wartan@gmail.com>
2024-07-03 19:43:42 +0330

    user clientarea skills in profile2221

--- app/Livewire/Front/Panel/Components/Profile.php
+++ app/Livewire/Front/Panel/Components/Profile.php
@@ -67,7 +67,7 @@ class Profile extends Component
 
             $user = User::find(auth()->id());
 
-            $user->skills()->attach($skill->id);
+            $user->skills()->attach($this->skill);
             $this->alert('success','مهارت جدید برای شما ثبت شد',['position'=>'center']);
         }
         catch(Throwable $e)
