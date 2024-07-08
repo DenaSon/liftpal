56f059d7d0e82dc21b6b97828bfee71ab794c287
milad <asadpour.milad@gmail.com>
2024-07-08 09:48:24 +0330

    compelet favorite page

--- resources/views/livewire/front/panel/components/favorite.blade.php
+++ resources/views/livewire/front/panel/components/favorite.blade.php
@@ -1,19 +1,63 @@
-<div class="card card-hover card-horizontal border-0 shadow-sm mb-4 mt-2">
-    <div class="card-img-top position-relative" style="background-image: url({{ asset('assets/img/car-finder/catalog/16.jpg') }});"><a class="stretched-link"
-                                                                                                                                       href="real-estate-single-v1.html"></a>
-        <div class="position-absolute start-0 top-0 pt-3 ps-3"><span class="d-table badge bg-success mb-1">فعال</span><span class="d-table badge bg-info">جدید</span></div>
-        <div class="position-absolute end-0 top-0 pt-3 pe-3 zindex-5">
-            <button class="btn btn-icon btn-light btn-xs text-primary rounded-circle shadow-sm" type="button" data-bs-toggle="tooltip" data-bs-placement="right"
-                    aria-label="حذف از مورد علاقه" data-bs-original-title="حذف از مورد علاقه"><i class="fi-heart-filled"></i></button>
+<div>
+
+    <div class="card card-hover card-horizontal border-0 shadow-sm mb-4 mt-3">
+        <div class="card-img-top  d-flex justify-content-center ">
+            <img src="{{ asset('assets/img/car-finder/about/03.png') }}"  style="max-height: 200px;" >
+
+        </div>
+        <div class="card-body  pb-3">
+            <div class="text-center">
+            <h3 class="h6 mb-2mt-2 fs-base "><a class="nav-link " href="#">موتور اسانسور مدل xgtdfgyt</a></h3>
+            <p class="mb-2mt-2 fs-sm ">دسته محصول(موتور برند)</p>
+            <div class="mb-3"><i class="fi-cash mt-n1 me-2 lead align-middle opacity-70"></i> قیمت &nbsp;<span>2555555</span> </div>
+            </div>
+            <div class="  d-grid gap-2  border-top d-md-block">
+                    <div class="container mb-0 row   mt-3 justify-content-between">
+                <button class="col-3 btn btn-outline-primary" type="button"><i class="fi-trash"></i></button>
+                <button class="col-8 btn btn-outline-primary" type="button">خرید</button>
+                    </div>
+            </div>
         </div>
     </div>
-    <div class="card-body position-relative pb-3">
-        <h4 class="mb-1 fs-sm fw-normal text-uppercase text-primary">فروش</h4>
-        <h3 class="h6 mb-2 fs-base"><a class="nav-link stretched-link" href="real-estate-single-v1.html">اقامتگاه ویلایی | 200 متر مربع</a></h3>
-        <p class="mb-2 fs-sm text-muted">ایران، تهران ، خیابان آزادی</p>
-        <div><i class="fi-cash mt-n1 me-2 lead align-middle opacity-70"></i>شروع قیمت از 1260000 ت</div>
-        <div class="d-flex align-items-center justify-content-center justify-content-sm-start border-top pt-3 pb-2 mt-3 text-nowrap"><span
-                class="d-inline-block me-4 fs-sm">3<i class="fi-bed ms-1 mt-n1 fs-lg text-muted"></i></span><span class="d-inline-block me-4 fs-sm">2<i
-                    class="fi-bath ms-1 mt-n1 fs-lg text-muted"></i></span><span class="d-inline-block fs-sm">2<i class="fi-car ms-1 mt-n1 fs-lg text-muted"></i></span></div>
+
+    <div class="card card-hover card-horizontal border-0 shadow-sm mb-4 mt-3">
+        <div class="card-img-top  d-flex justify-content-center ">
+            <img src="{{ asset('assets/img/car-finder/categories/03.png') }}"  style="max-height: 200px;" >
+
+        </div>
+        <div class="card-body  pb-3">
+            <div class="text-center">
+                <h3 class="h6 mb-2mt-2 fs-base "><a class="nav-link " href="#">موتور اسانسور مدل xgtdfgyt</a></h3>
+                <p class="mb-2mt-2 fs-sm ">دسته محصول(موتور برند)</p>
+                <div class="mb-3"><i class="fi-cash mt-n1 me-2 lead align-middle opacity-70"></i> قیمت &nbsp;<span>2555555</span> </div>
+            </div>
+            <div class="  d-grid gap-2  border-top d-md-block">
+                <div class="container mb-0 row   mt-3 justify-content-between">
+                    <button class="col-3 btn btn-outline-primary" type="button"><i class="fi-trash"></i></button>
+                    <button class="col-8 btn btn-outline-primary" type="button">خرید</button>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <div class="card card-hover card-horizontal border-0 shadow-sm mb-4 mt-3">
+        <div class="card-img-top d-flex justify-content-center ">
+            <img src="{{ asset('assets/img/car-finder/categories/04.png') }}"  style="max-height: 200px;" >
+
+        </div>
+        <div class="card-body  pb-3">
+            <div class="text-center">
+                <h3 class="h6 mb-2mt-2 fs-base "><a class="nav-link " href="#">موتور اسانسور مدل xgtdfgyt</a></h3>
+                <p class="mb-2mt-2 fs-sm ">دسته محصول(موتور برند)</p>
+                <div class="mb-3"><i class="fi-cash mt-n1 me-2 lead align-middle opacity-70"></i> قیمت &nbsp;<span>2555555</span> </div>
+            </div>
+            <div class="  d-grid gap-2  border-top d-md-block">
+                <div class="container mb-0 row   mt-3 justify-content-between">
+                    <button class="col-3 btn btn-outline-primary" type="button"><i class="fi-trash"></i></button>
+                    <button class="col-8 btn btn-outline-primary" type="button">خرید</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
