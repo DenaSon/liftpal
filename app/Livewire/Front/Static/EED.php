96f7882262c37ca1e4b8e214e975feb6021d0db8
Mohammad Asadi <Mohammad.wartan@gmail.com>
2024-07-10 00:07:57 +0330

    EED Modify

---
+++ app/Livewire/Front/Static/EED.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Front\Static;
+
+use Livewire\Component;
+
+class EED extends Component
+{
+    public function render()
+    {
+        return view('livewire.front.static.e-e-d');
+    }
+}
