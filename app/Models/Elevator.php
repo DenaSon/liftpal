c66a727ac10e60475cd2a746e180fdd4c0491c6a
Mohammad Asadi <Mohammad.wartan@gmail.com>
2024-07-16 21:18:12 +0330

    Database Updates for Elevator

--- app/Models/Elevator.php
+++ app/Models/Elevator.php
@@ -18,4 +18,37 @@ class Elevator extends Model
     {
         return $this->hasOneThrough(User::class, Building::class, 'id', 'id', 'building_id', 'user_id');
     }
+
+    public function isActive(): bool
+    {
+        return $this->type === 'active';
+    }
+    public function isUnderMaintenance(): bool
+    {
+        return $this->type === 'maintenance';
+    }
+    public function isPassenger(): bool
+    {
+        return $this->type === 'passenger';
+    }
+
+    public function isFreight(): bool
+    {
+        return $this->type === 'freight';
+    }
+
+    public function isService(): bool
+    {
+        return $this->type === 'service';
+    }
+
+    public function isPanoramic(): bool
+    {
+        return $this->type === 'panoramic';
+    }
+
+    public function isOther(): bool
+    {
+        return $this->type === 'other';
+    }
 }
