3713df783815264acedb452fee25ffbc525ef3ad
milad <asadpour.milad@gmail.com>
2024-08-06 12:30:33 +0330

    second section of repair

--- resources/views/livewire/front/home-inc/hero-slider.blade.php
+++ resources/views/livewire/front/home-inc/hero-slider.blade.php
@@ -4,7 +4,12 @@
             "items": 3,
             "controls": false,
             "autoplay": true,
-            "autoplayTimeout": 3000,
+            "autoplayTimeout": 5000,
+            "edgePadding": 50,
+            "swipeAngle": false,
+            "speed": 4500,
+            "arrowKeys": true,
+
             "responsive": {
                 "0": {"items": 1},
                 "1024": {"items": 2},
@@ -12,43 +17,33 @@
             }
          }'>
 
-        <div class="col">
-            <div class="h-100 pb-2 pb-sm-3 px-sm-3 text-center">
+        <div class="col d-flex justify-content-center align-items-center">
+            <div class=" h-100 pb-2 pb-sm-3 px-sm-3 text-center">
 
-                <img w-100 class="d-block mx-auto my-3" src="{{ asset('assets/img/car-finder/home/hero-img.png') }}"
+                <img w-100 class="d-block mx-auto my-3" src="{{ asset('assets/img/home-img/hero-image.jpg') }}"
                      alt="Illustration">
 
             </div>
         </div>
 
 
+        <div class="col d-flex justify-content-center align-items-center">
+            <div class="  h-100 pb-2 pb-sm-3 px-sm-3 text-center"><img
+                        class="d-block mx-auto my-3"
+                        src="{{ asset('assets/img/footer/elevator.png') }}"
+                        alt="Illustration">
+
+            </div>
+
+        </div>
         <div class="col">
-            <div class=" border-0 h-100 pb-2 pb-sm-3 px-sm-3 text-center"><img
-                    class="d-block mx-auto my-3"
-                    src="{{ asset('assets/img/real-estate/illustrations/find.svg') }}"
-                   alt="Illustration">
+            <div class=" h-100 pb-2 pb-sm-3 px-sm-3 text-center"><img
+                        class="d-block mx-auto my-3"
+                        src="{{ asset('assets/img/real-estate/illustrations/calculator.svg') }}"
+                        alt="Illustration">
 
             </div>
         </div>
-        <div class="col">
-            <div class="border-0 h-100 pb-2 pb-sm-3 px-sm-3 text-center"><img
-                    class="d-block mx-auto my-3"
-                    src="{{ asset('assets/img/real-estate/illustrations/calculator.svg') }}"
-                     alt="Illustration">
-
-            </div>
-        </div>
-        <div class="col">
-            <div class="border-0 h-100 pb-2 pb-sm-3 px-sm-3 text-center"><img
-                    class="d-block mx-auto my-3"
-                    src="{{ asset('assets/img/real-estate/illustrations/support.svg') }}"
-                   alt="Illustration">
-
-            </div>
-        </div>
-
-
-
 
 
     </div>
