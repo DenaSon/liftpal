06a5fb72d462b66231418032a836309e04a9024d
Denason <mohammad.wartan@gmail.com>
2024-08-06 11:23:38 +0330

    add slider to header and split slider to new blade file

---
+++ resources/views/livewire/front/home-inc/hero-slider.blade.php
@@ -0,0 +1,65 @@
+<div class="tns-carousel-wrapper tns-nav-outside tns-nav-outside-flush mx-n2" dir="ltr">
+    <div class="tns-carousel-inner row gx-4 mx-0 py-3"
+         data-carousel-options="{&quot;items&quot;: 4, &quot;controls&quot;: true, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1},&quot;500&quot;:{&quot;items&quot;:2},&quot;768&quot;:{&quot;items&quot;:3}}}">
+
+        <div class="col">
+            <div class="card card-hover border-0 h-100 pb-2 pb-sm-3 px-sm-3 text-center">
+
+                <img   class="d-block mx-auto my-3" src="{{ asset('admin/assets/images/products/product-11.jpg') }}"
+                       width="256" alt="Illustration">
+
+            </div>
+        </div>
+
+
+        <div class="col">
+            <div class="card card-hover border-0 h-100 pb-2 pb-sm-3 px-sm-3 text-center"><img
+                    class="d-block mx-auto my-3"
+                    src="{{ asset('assets/img/real-estate/illustrations/find.svg') }}"
+                    width="256" alt="Illustration">
+
+            </div>
+        </div>
+        <div class="col">
+            <div class="card card-hover border-0 h-100 pb-2 pb-sm-3 px-sm-3 text-center"><img
+                    class="d-block mx-auto my-3"
+                    src="{{ asset('assets/img/real-estate/illustrations/calculator.svg') }}"
+                    width="256" alt="Illustration">
+
+            </div>
+        </div>
+        <div class="col">
+            <div class="card card-hover border-0 h-100 pb-2 pb-sm-3 px-sm-3 text-center"><img
+                    class="d-block mx-auto my-3"
+                    src="{{ asset('assets/img/real-estate/illustrations/support.svg') }}"
+                    width="256" alt="Illustration">
+
+
+            </div>
+        </div>
+
+
+        <div class="col">
+            <div class="card card-hover border-0 h-100 pb-2 pb-sm-3 px-sm-3 text-center"><img
+                    class="d-block mx-auto my-3"
+                    src="{{ asset('assets/img/real-estate/illustrations/online-shopping.svg') }}"
+                    width="256" alt="Illustration">
+                <div class="card-body">
+                    <h2 class="h5 card-title">فروشگاه</h2>
+                    <p class="card-text fs-sm">
+
+
+                        لیفت‌پال به عنوان یک سامانه جامع، در کنار خدمات متنوعی که در زمینه آسانسور ارائه می دهد،
+                        فروشگاه آنلاین قطعات و ابزار آسانسور را نیز راه اندازی کرده است.
+
+                    </p>
+                </div>
+                <div class="card-footer pt-0 border-0"><a wire:navigate.hover
+                                                          class="btn btn-outline-primary stretched-link"
+                                                          href="{{ route('shop') }}"> فروشگاه </a></div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
