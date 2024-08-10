c2f3111feaebe1e172f65ff68f08a5279db2b814
Denason <mohammad.wartan@gmail.com>
2024-08-10 09:51:11 +0330

    correct slider

--- resources/views/livewire/front/home-inc/hero-slider.blade.php
+++ resources/views/livewire/front/home-inc/hero-slider.blade.php
@@ -1,48 +1,42 @@
 <div class="tns-carousel-wrapper tns-nav-outside tns-nav-outside-flush" dir="ltr">
     <div class="tns-carousel-inner"
-         data-carousel-options=
-                 '{
-            "controls": false,
-            "autoplay": true,
-            "autoplayTimeout": 5000,
-            "swipeAngle": false,
-            "speed": 4500,
-            "arrowKeys": true,
-
-            "responsive": {
-    "0": {"items": 1},
-    "768": {"items": 1},
-    "1024": {"items": 1}
-    }
+         data-carousel-options='{
+        "items": 1,
+        "controls": false,
+        "autoplay": true,
+        "autoplayTimeout": 5000,
+        "swipeAngle": false,
+        "speed": 4500,
+        "arrowKeys": true,
+        "loop": true,
+        "responsive": {
+            "0": {"items": 1},
+            "768": {"items": 1},
+            "1024": {"items": 1}
+        }
     }'>
 
 
-        {{--            <div class="col h-100 pb-2 pb-sm-3 px-sm-3 text-center">--}}
+
+    {{--            <div class="col h-100 pb-2 pb-sm-3 px-sm-3 text-center">--}}
 
         {{--                <img w-100 class="d-block mx-auto my-3" src="{{ asset('assets/img/home-img/hero-image.jpg') }}"--}}
         {{--                     alt="Illustration">--}}
 
         {{--            </div>--}}
+        <div class="col mx-2 position-relative" style="height: 320px;">
+            <img src="{{ asset('assets/img/home-img/hero-image.jpg') }}" class="" alt="Hero Image" style="height: 100%; width: 100%; object-fit: cover;">
 
-        <div class="col  mx-2">
-            <div>
-                <img src="{{ asset('assets/img/home-img/hero-image.jpg') }}" class="h-75" ></div>
-
-            <div class=" bg-dark bg-opacity-50 text-white mt-3 rounded p-3">
-                <h5 class="">لیفت پال</h5>
-                <p class=" fs-sm">یا آماده‌اید تا تجربه‌ای جدید از مدیریت آسانسورهای ساختمان خود داشته باشید؟ سامانه آنلاین ما، یک پلتفرم هوشمند و جامع است که تمامی نیازهای شما را پوشش می‌دهد.</p>
+            <div class="menu-overlay bg-dark bg-opacity-25 text-white rounded p-3 position-absolute bottom-0 w-100">
+                <h5>لیفت پال</h5>
+                <p class="fs-sm">
+                    آیا آماده‌اید تا تجربه‌ای جدید از مدیریت آسانسورهای ساختمان خود داشته باشید؟ سامانه آنلاین ما، یک پلتفرم هوشمند و جامع است که تمامی نیازهای شما را پوشش می‌دهد.
+                </p>
                 <a href="#" class="btn btn-sm btn-primary">لیفت پال</a>
             </div>
         </div>
 
-        <div class="col card mx-2">
-            <img src="{{ asset('assets/img/home-img/sky-profile-picture-2.jpg') }}" class="card-img-top" alt="Card image" >
-            <div class="card-body">
-                <h5 class="card-title">لیفت پال</h5>
-                <p class="card-text fs-sm">یا آماده‌اید تا تجربه‌ای جدید از مدیریت آسانسورهای ساختمان خود داشته باشید؟ سامانه آنلاین ما، یک پلتفرم هوشمند و جامع است که تمامی نیازهای شما را پوشش می‌دهد.</p>
-                <a href="#" class="btn btn-sm btn-primary">لیفت پال</a>
-            </div>
-        </div>
+
 
 
         <div class="col card mx-2">
