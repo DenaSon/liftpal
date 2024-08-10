67f01fdb984ca57fe0dca23e8ca5f3bfbfca195e
Denason <mohammad.wartan@gmail.com>
2024-08-10 15:40:05 +0330

    hero slider modify

--- resources/views/livewire/front/home-inc/hero-slider.blade.php
+++ resources/views/livewire/front/home-inc/hero-slider.blade.php
@@ -1,10 +1,11 @@
 <div class="tns-carousel-wrapper tns-nav-outside tns-nav-outside-flush" dir="ltr">
     <div class="tns-carousel-inner"
          data-carousel-options='{
+
         "items": 1,
-        "controls": false,
-        "autoplay": true,
-        "autoplayTimeout": 5000,
+        "controls": true,
+        "autoplay": false,
+        "autoplayTimeout": 6500,
         "swipeAngle": false,
         "speed": 4500,
         "arrowKeys": true,
@@ -18,47 +19,46 @@
 
 
         <div class="col mx-2 position-relative" style="height: 400px;">
-            <img src="{{ asset('assets/img/home-img/teqnician.jpg') }}" class="rounded" alt="" style="height: 100%; width: 100%; object-fit: cover;">
+            <img src="{{ asset('media/eleliftvator.jpg') }}" class="rounded" alt="" style="height: 100%; width: 100%; object-fit: cover;">
 
-            <div class=" menu-overlay bg-dark bg-opacity-25 text-white rounded p-3 position-absolute bottom-0 w-100">
-                <h5 class="text-center text-primary">یافتن تکنسین</h5>
+            <div class=" menu-overlay bg-dark bg-opacity-50 text-white rounded p-3 position-absolute bottom-0 w-100">
+
                 <p class="text-center fs-sm">
                     آیا آماده‌اید تا تجربه‌ای جدید از مدیریت آسانسورهای ساختمان خود داشته باشید؟ سامانه آنلاین ما، یک پلتفرم هوشمند و جامع است که تمامی نیازهای شما را پوشش می‌دهد.
                 </p>
                 <div class="d-flex justify-content-center">
-                <a href="#" class="btn btn-sm btn-primary ">جستجو</a>
+                <a href="#" class="btn btn-sm btn-outline-light w-25 ">جستجو</a>
                 </div>
             </div>
         </div>
 
         <div class="col mx-2 position-relative" style="height: 400px;">
-            <img src="{{ asset('assets/img/home-img/hero-image.jpg') }}" class="rounded" alt="" style="height: 100%; width: 100%; object-fit: cover;">
+            <img src="{{ asset('media/teamwork.jpg') }}" class="rounded" alt="" style="height: 100%; width: 100%; object-fit: cover;">
 
-            <div class=" menu-overlay bg-dark bg-opacity-25 text-white rounded p-3 position-absolute bottom-0 w-100">
-                <h5 class="text-center text-primary">لیفت پال</h5>
+            <div class=" menu-overlay bg-dark bg-opacity-50 text-white rounded p-3 position-absolute bottom-0 w-100">
+
                 <p class="text-center fs-sm">
                     آیا آماده‌اید تا تجربه‌ای جدید از مدیریت آسانسورهای ساختمان خود داشته باشید؟ سامانه آنلاین ما، یک پلتفرم هوشمند و جامع است که تمامی نیازهای شما را پوشش می‌دهد.
                 </p>
                 <div class="d-flex justify-content-center">
-                <a href="#" class="btn btn-sm btn-primary ">لیفت پال</a>
+                <a href="#" class="btn btn-sm btn-outline-light w-25 ">لیفت پال</a>
                 </div>
             </div>
         </div>
 
         <div class="col mx-2 position-relative" style="height: 400px;">
-            <img src="{{ asset('assets/img/home-img/sabt-sakhteman.jpg') }}" class="rounded" alt="" style="height: 100%; width: 100%; object-fit: cover;">
+            <img src="{{ asset('media/sakhteman.jpg') }}" class="rounded" alt="" style="height: 100%; width: 100%; object-fit: cover;">
 
-            <div class=" menu-overlay bg-dark bg-opacity-25 text-white rounded p-3 position-absolute bottom-0 w-100">
-                <h5 class="text-center text-primary">ثبت ساختمان</h5>
-                <p>
+            <div class=" menu-overlay bg-dark bg-opacity-50 text-white rounded p-3 position-absolute bottom-0 w-100">
+
+                <p class="text-center">
                     آیا آماده‌اید تا تجربه‌ای جدید از مدیریت آسانسورهای ساختمان خود داشته باشید؟ سامانه آنلاین ما، یک پلتفرم هوشمند و جامع است که تمامی نیازهای شما را پوشش می‌دهد.
                 </p>
                 <div class="d-flex justify-content-center">
-                <a href="#" class="btn btn-sm btn-primary ">ثبت</a>
+                <a href="#" class="btn btn-sm btn-outline-light w-25 ">ثبت</a>
                 </div>
             </div>
         </div>
-
 
 
     </div>
--- resources/views/livewire/front/panel/components/building.blade.php
+++ resources/views/livewire/front/panel/components/building.blade.php
@@ -1,15 +1,10 @@
 <div>
-    <a href="?page=building&action=create"> افزودن </a>
 
-
-<hr>
-
-@if(request()->input('action'))
 
     @include('livewire.front.panel.components.building-inc.create')
 
-@else
 
-@endif
+
+
 
 </div>
