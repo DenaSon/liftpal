e230dc31b308f3a4394eb4d8f6d68718a70da9e7
milad <asadpour.milad@gmail.com>
2024-08-10 19:19:17 +0330

    second section of repair

--- resources/views/livewire/front/panel/components/notifications.blade.php
+++ resources/views/livewire/front/panel/components/notifications.blade.php
@@ -1,3 +1,82 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="container">
+    <div class=" mb-5">
+        <h1 class="h2">لیست اطلاعیه ها</h1>
+        <p class="pt-1 mb-4">دریافت به روزرسانی های واقعی در مورد خدمات مورد نیاز ساختمان شما</p>
+        <!-- Nav tabs-->
+        <ul class="nav nav-tabs flex-column flex-sm-row align-items-stretch align-items-sm-start border-bottom mb-4" role="tablist">
+            <li class="nav-item me-sm-3 mb-3" role="presentation"><a class="nav-link text-center active" href="#notifications-rent" data-bs-toggle="tab" role="tab" aria-controls="notifications-rent" aria-selected="true">نوتیفیکیشن های خدمات جدید</a></li>
+            <li class="nav-item mb-3" role="presentation"><a class="nav-link text-center" href="#notifications-sale" data-bs-toggle="tab" role="tab" aria-controls="notifications-sale" aria-selected="false" tabindex="-1">نوتیفیکیشن های دعوت به کار</a></li>
+        </ul>
+        <!-- Tabs content-->
+        <div class="tab-content py-2" id="notification-settings">
+            <!-- Rent notifications tab-->
+            <div class="tab-pane fade active show" id="notifications-rent" role="tabpanel">
+                <div class="d-flex justify-content-between mb-4">
+                    <div class="me-2">
+                        <h6 class="mb-1">آگهی های جدید خدمات</h6>
+                        <p class="fs-sm mb-0"> اجاره های جدید که با شما مطابقت دارد</p>
+                    </div>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="new-rental" checked="">
+                        <label class="form-check-label" for="new-rental"></label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between mb-4">
+                    <div class="me-2">
+                        <h6 class="mb-1">به روز رسانی وضعیت اجاره</h6>
+                        <p class="fs-sm mb-0">به روزرسانی هایی مانند تغییرات قیمت و غیرفعال شدن آگهی </p>
+                    </div>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="rental-update" checked="">
+                        <label class="form-check-label" for="rental-update"></label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between mb-4">
+                    <div class="me-2">
+                        <h6 class="mb-1">پیشنهادات سایت</h6>
+                        <p class="fs-sm mb-0">اجاره هایی که فکر می کنیم دوست دارید. این توصیه ها ممکن است کمی خارج از معیارهای جستجوی شما باشد</p>
+                    </div>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="rental-recomendation">
+                        <label class="form-check-label" for="rental-recomendation"></label>
+                    </div>
+                </div>
+            </div>
+            <!-- Sale notifications tab-->
+            <div class="tab-pane fade" id="notifications-sale" role="tabpanel">
+                <div class="d-flex justify-content-between mb-4">
+                    <div class="me-2">
+                        <h6 class="mb-1">آگهی های جدید فروش</h6>
+                        <p class="fs-sm mb-0">فروش های جدید متناسب با شما </p>
+                    </div>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="new-sale" checked="">
+                        <label class="form-check-label" for="new-sale"></label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between mb-4">
+                    <div class="me-2">
+                        <h6 class="mb-1">به روز رسانی وضعیت اجاره</h6>
+                        <p class="fs-sm mb-0">به روزرسانی هایی مانند تغییرات قیمت و غیرفعال شدن آگهی </p>
+                    </div>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="sale-update">
+                        <label class="form-check-label" for="sale-update"></label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between mb-4">
+                    <div class="me-2">
+                        <h6 class="mb-1">پیشنهادات سایت</h6>
+                        <p class="fs-sm mb-0">اجاره هایی که فکر می کنیم دوست دارید. این توصیه ها ممکن است کمی خارج از معیارهای جستجوی شما باشد</p>
+                    </div>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="sale-recomendation">
+                        <label class="form-check-label" for="sale-recomendation"></label>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
 </div>
--- resources/views/livewire/front/panel/components/support-inc/responses.blade.php
+++ resources/views/livewire/front/panel/components/support-inc/responses.blade.php
@@ -1,33 +1,18 @@
-بخش مشاهده پرسش ها و پاسخ های ارسال و دریافت شده
-
-
 <div>
 
-<div class="container shadow-lg ms-auto bg-secondary mb-3 mt-5 border rounded-2  p-3 width-auto h-auto"><i class="fi-help info">&nbsp</i>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam assumenda aut commodi corporis culpa cum dicta dolor doloremque doloribus ea enim est harum impedit inventore ipsam, itaque iure laudantium maxime mollitia natus necessitatibus neque nihil nobis numquam perferendis praesentium quae quaerat quia, quo reiciendis reprehenderit suscipit, tempore unde veniam voluptatibus voluptatum. A facilis neque perspiciatis quibusdam quos ratione repudiandae saepe soluta suscipit. Accusamus aperiam asperiores commodi consequuntur corporis cumque
-    eaque
-    enim ipsum itaque natus necessitatibus,
-    nostrum, obcaecati omnis quas repellat, reprehenderit.
-</div>
-
-
-
-    <div class="container shadow-lg ms-5 bg-white mb-3 mt-5 border rounded-2  p-3 width-auto h-auto"><i class="fi-info-circle ">&nbsp</i><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam assumenda aut commodi corporis culpa cum dicta dolor doloremque doloribus ea enim est harum impedit inventore ipsam, itaque iure laudantium maxime mollitia natus necessitatibus neque nihil nobis numquam perferendis praesentium quae quaerat quia, quo reiciendis reprehenderit suscipit, tempore unde veniam voluptatibus voluptatum. A facilis neque perspiciatis quibusdam quos ratione repudiandae saepe soluta suscipit. Accusamus aperiam asperiores commodi consequuntur corporis cumque eaque enim
-        ipsum itaque natus necessitatibus,
-        nostrum, obcaecati omnis quas repellat, reprehenderit.
+    <div class="container ms-5 bg-white mb-3 mt-5 border rounded-2 shadow-lg p-3 width-auto h-auto"><i class="fi-corner-down-left mb-3 ms-3"></i><br>
+        <p>
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار
+            گیرد.
+        </p>
     </div>
 
 
-    <div class="container  ms-5 bg-white mb-3 mt-5 border rounded-2 shadow-lg p-3 width-auto h-auto"><i class="fi-corner-down-left ">&nbsp</i><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam assumenda aut commodi corporis culpa cum dicta dolor doloremque doloribus ea enim est harum impedit inventore ipsam, itaque iure laudantium maxime mollitia natus necessitatibus neque nihil nobis numquam perferendis praesentium quae quaerat quia, quo reiciendis reprehenderit suscipit, tempore unde veniam voluptatibus voluptatum. A facilis neque perspiciatis quibusdam quos ratione repudiandae saepe soluta suscipit. Accusamus aperiam asperiores commodi consequuntur
-        corporis cumque eaque enim
-        ipsum itaque natus necessitatibus,
-        nostrum, obcaecati omnis quas repellat, reprehenderit.
-    </div>
 
-
-    <!-- Testimonial inside card: Basic -->
     <div class="card border-0 mt-5 shadow-sm ms-5">
         <blockquote class="blockquote card-body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده
+                قرار گیرد.</p>
 
         </blockquote>
     </div>
