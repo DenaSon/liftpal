63121f6b9acebac4976e179fe4f0626dde57dbda
milad <asadpour.milad@gmail.com>
2024-08-11 11:07:21 +0330

    second section of repair

--- resources/views/livewire/front/panel/components/building.blade.php
+++ resources/views/livewire/front/panel/components/building.blade.php
@@ -2,7 +2,6 @@
 
     <div class=" mt-3">
 
-
         <div class="border rounded shadow-sm p-4 mt-3">
             <div class="container position-relative">
                 <div class="row justify-content-center">
@@ -15,6 +14,7 @@
                 </div>
 
             </div>
+
 
             <div class="container mt-3">
 
@@ -40,10 +40,7 @@
 
 
                                 <div class="modal-body">
-
-
                                     <!-- Caff Milano | Edit From Here -->
-
 
                                     <div class="form-floating mb-3">
                                     <textarea class="form-control" placeholder="آدرس ساختمان"
@@ -102,71 +99,77 @@
                                     </div>
 
 
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-success btn-xs w-25">ثبت</button>
-                                <button type="button" class="btn btn-primary btn-xs" data-bs-dismiss="modal">بستن
-                                </button>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-success btn-xs w-25">ثبت</button>
+                                    <button type="button" class="btn btn-primary btn-xs" data-bs-dismiss="modal">بستن
+                                    </button>
 
+                                </div>
                             </div>
                         </div>
                     </div>
+
+
                 </div>
 
+                <div class="row container mt-3 collapse" id="showMorebuilding" style="">
+                    <!-- List group with icons and badges -->
+                    <ul class="list-group">
 
-            </div>
-
-            <div class="row container mt-3 collapse" id="showMorebuilding" style="">
-                <!-- List group with icons and badges -->
-                <ul class="list-group">
-
-                    <li wire:key="2" class="list-group-item d-flex justify-content-between align-items-center  ">
+                        <li wire:key="2" class="list-group-item d-flex justify-content-between align-items-center  ">
 <span>
 <i class="fi-apartment text-success me-2"></i>
 ساختمان 1
 </span>
-                        <a href="#" class="">
-                            <i class=" btn-xs fi fi-trash"></i>
-                        </a>
-                    </li>
-                    <li wire:key="3" class="list-group-item d-flex justify-content-between align-items-center  ">
+                            <a href="#" class="">
+                                <i class=" btn-xs fi fi-trash"></i>
+                            </a>
+                        </li>
+                        <li wire:key="3" class="list-group-item d-flex justify-content-between align-items-center  ">
 <span>
 <i class="fi-apartment text-success me-2"></i>
 ساختمان 2
 </span>
-                        <a href="#" class="">
-                            <i class=" btn-xs fi fi-trash"></i>
-                        </a>
-                    </li>
-                    <li wire:key="2" class="list-group-item d-flex justify-content-between align-items-center  ">
+                            <a href="#" class="">
+                                <i class=" btn-xs fi fi-trash"></i>
+                            </a>
+                        </li>
+                        <li wire:key="2" class="list-group-item d-flex justify-content-between align-items-center  ">
 <span>
 <i class="fi-apartment text-success me-2"></i>
 ساختمان 3
 </span>
-                        <a href="#" class="">
-                            <i class=" btn-xs fi fi-trash"></i>
-                        </a>
-                    </li>
-                </ul>
+                            <a href="#" class="">
+                                <i class=" btn-xs fi fi-trash"></i>
+                            </a>
+                        </li>
+                    </ul>
 
-            </div>
-
-            <div class="container mt-2">
-                <div class="d-flex flex-column flex-md-row align-items-center align-items-md-start">
-
-                    <a class="collapse-label d-inline-block fs-md fw-bold text-success text-decoration-none pt-2 pb-3 mx-auto mx-md-0 ms-md-2 collapsed"
-                       href="#showMorebuilding" data-bs-toggle="collapse" data-bs-label-collapsed="ساختمان های ثبت شده"
-                       data-bs-label-expanded="بستن" role="button" aria-expanded="false"
-                       aria-controls="showMorebuilding">
-                        <i class="fi-arrow-down me-2"></i>
-                    </a>
                 </div>
+
+                <div class="container mt-2">
+                    <div class="d-flex flex-column flex-md-row align-items-center align-items-md-start">
+
+                        <a class="collapse-label d-inline-block fs-md fw-bold text-success text-decoration-none pt-2 pb-3 mx-auto mx-md-0 ms-md-2 collapsed"
+                           href="#showMorebuilding" data-bs-toggle="collapse" data-bs-label-collapsed="ساختمان های ثبت شده"
+                           data-bs-label-expanded="بستن" role="button" aria-expanded="false"
+                           aria-controls="showMorebuilding">
+                            <i class="fi-arrow-down me-2"></i>
+                        </a>
+                    </div>
+                </div>
+
             </div>
 
         </div>
+    </div>
 
 
-        <div class="border rounded shadow-sm p-4 mt-3">
+
+
+
+    <div class="border rounded shadow-sm p-4 mt-3">
             <div class="container position-relative">
                 <div class="row justify-content-center">
                     <div class="col-md-6 d-flex justify-content-center">
