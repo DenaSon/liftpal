52aa11c9548adeb814e7d31975b7eccece58e68d
Denason <mohammad.wartan@gmail.com>
2024-08-14 22:51:46 +0330

    create fault views

---
+++ app/Livewire/Front/Panel/Components/FaultAlert.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Front\Panel\Components;
+
+use Livewire\Component;
+
+class FaultAlert extends Component
+{
+    public function render()
+    {
+        return view('livewire.front.panel.components.fault-alert');
+    }
+}
---
+++ resources/views/livewire/front/panel/components/fault-alert.blade.php
@@ -0,0 +1,11 @@
+<div>
+
+
+   <div class="text-center">
+
+       Nazanin 16 Shz
+
+   </div>
+
+
+</div>
