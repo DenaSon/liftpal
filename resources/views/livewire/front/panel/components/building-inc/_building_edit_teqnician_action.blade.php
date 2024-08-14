aaead7119bb4c9f7785e0e0ef7ef6e6dd406d14a
milad <asadpour.milad@gmail.com>
2024-08-14 11:44:54 +0330

    set modal of send message to teqnician

---
+++ resources/views/livewire/front/panel/components/building-inc/_building_edit_teqnician_action.blade.php
@@ -0,0 +1,46 @@
+<div wire:ignore class="modal fade" id="teqnician-action-building" data-bs-backdrop="static" data-bs-keyboard="false"
+     tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header ">
+                <h1 class="modal-title fs-5 bg-soft-success" id="staticBackdropLabel">ارسال پیام به تکنسین</h1>
+                <button type="button" class="btn-close me-0" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+
+                <div class="mt-2">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>انتخاب تکنسین</option>
+                        <option value="1">شماره 1</option>
+                        <option value="2">شماره 2</option>
+                        <option value="3">شماره 3</option>
+                    </select>
+
+                </div>
+
+
+                <div class="mt-2">
+
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+
+
+                </div>
+
+
+
+            </div>
+
+            <div class="modal-footer d-flex justify-content-sm-center justify-content-md-end">
+                <button type="button" class="btn btn-success btn-xs w-25">ارسال</button>
+                <button type="button" class="btn btn-primary btn-xs" data-bs-dismiss="modal">بستن</button>
+            </div>
+        </div>
+    </div>
+</div>
