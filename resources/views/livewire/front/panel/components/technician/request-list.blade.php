68f66f91c3988d38f89cf92b4ad7afc847fa3bd9
milad <asadpour.milad@gmail.com>
2024-08-18 14:12:36 +0330

    setup request page

--- resources/views/livewire/front/panel/components/technician/request-list.blade.php
+++ resources/views/livewire/front/panel/components/technician/request-list.blade.php
@@ -1,3 +1,89 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+
+<div class="row d-flex justify-content-around">
+    <div class="card col-md-5 mt-3 shadow-lg ">
+
+        <div class="list-group m-2">
+
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>
+      <i class="fi-chat-left text-muted me-2"></i>
+      تعداد درخواست ها
+    </span>
+                <span class="badge bg-success">14</span>
+            </li>
+
+            <li href="#" class="list-group-item list-group-item-action">
+    <span class="fs-xs muted d-none d-md-inline-block">ساختمان بهار کوهپایه 3</span>
+    </li>
+            <li href="#" class="list-group-item list-group-item-action "><span class="fs-xs muted d-none d-md-inline-block">با مدیریت اقای اسدپور</span>
+            </li>
+
+            <li href="#" class="list-group-item list-group-item-action "><span class="fs-xs muted d-none d-md-inline-block">شماره تماس 09380446842</span>
+            </li>
+
+            <li href="#" class="list-group-item list-group-item-action "><span class="fs-xs muted d-none d-md-inline-block">به دلیل خرابی بسته نشدن درب اسانسور</span>
+            </li>
+
+        </div>
+
+
+        <div class="row card-body d-flex justify-content-center">
+
+            <div class=" mt-5 d-flex justify-content-center">
+                <button type="button" class=" w-50 me-2 btn btn-success">
+                     تایید
+                </button>
+
+                <button type="button" class="  btn btn-primary" data-bs-toggle="button">رد کردن</button>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="card col-md-5 mt-3 shadow-lg ">
+
+        <div class="list-group m-2">
+
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>
+      <i class="fi-chat-left text-muted me-2"></i>
+      تعداد درخواست ها
+    </span>
+                <span class="badge bg-success">14</span>
+            </li>
+
+            <li href="#" class="list-group-item list-group-item-action">
+                <span class="fs-xs muted d-none d-md-inline-block">ساختمان بهار کوهپایه 3</span>
+            </li>
+            <li href="#" class="list-group-item list-group-item-action "><span class="fs-xs muted d-none d-md-inline-block">با مدیریت اقای اسدپور</span>
+            </li>
+
+            <li href="#" class="list-group-item list-group-item-action "><span class="fs-xs muted d-none d-md-inline-block">شماره تماس 09380446842</span>
+            </li>
+
+            <li href="#" class="list-group-item list-group-item-action "><span class="fs-xs muted d-none d-md-inline-block">به دلیل خرابی بسته نشدن درب اسانسور</span>
+            </li>
+
+        </div>
+
+
+        <div class="row card-body d-flex justify-content-center">
+
+            <div class=" mt-5 d-flex justify-content-center">
+                <button type="button" class=" w-50 me-2 btn btn-success">
+                    تایید
+                </button>
+
+                <button type="button" class="  btn btn-primary" data-bs-toggle="button">رد کردن</button>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+</div>
+
 </div>
