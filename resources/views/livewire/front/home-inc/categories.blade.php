b581da154bcd7ab807cf711b64d3c8355ec2415d
Denason <mohammad.wartan@gmail.com>
2024-08-21 16:13:14 +0330

    send technician alert sms

--- resources/views/livewire/front/home-inc/categories.blade.php
+++ resources/views/livewire/front/home-inc/categories.blade.php
@@ -3,23 +3,24 @@
 
     <div class="row row-cols-lg-4 row-cols-sm-2 row-cols-2 g-3 g-xl-4">
 
-        <div class="col"><a class="icon-box card card-body h-100 border-0 shadow card-hover h-100 text-center"
-                            href="">
+        <div class="col"><a  wire:navigate class="icon-box card card-body h-100 border-0 shadow card-hover h-100 text-center"
+                            href="{{ route('blogIndex',['cid'=>3]) }}">
                 <div class="icon-box-media bg-faded-primary text-primary rounded-circle mb-3 mx-auto"><i
                         class="fi-real-estate-house"></i></div>
                 <h3 class="icon-box-title fs-base mb-0">تاسیسات </h3></a></div>
-        <div class="col"><a class="icon-box card card-body h-100 border-0 shadow card-hover h-100 text-center"
-                            href="">
+        <div class="col"><a wire:navigate class="icon-box card card-body h-100 border-0 shadow card-hover h-100 text-center"
+                            href="{{ route('blogIndex',['cid'=>4]) }}">
                 <div class="icon-box-media bg-faded-primary text-primary rounded-circle mb-3 mx-auto"><i
                         class="fi-calendar"></i></div>
                 <h3 class="icon-box-title fs-base mb-0">تکنولوژی</h3></a></div>
-        <div class="col"><a class="icon-box card card-body h-100 border-0 shadow card-hover h-100 text-center"
-                            href="">
+        <div class="col">
+            <a wire:navigate class="icon-box card card-body h-100 border-0 shadow card-hover h-100 text-center"
+                            href="{{ route('blogIndex',['cid'=>1]) }}">
                 <div class="icon-box-media bg-faded-primary text-primary rounded-circle mb-3 mx-auto"><i
                         class="fi-billboard-house"></i></div>
                 <h3 class="icon-box-title fs-base mb-0">قوانین </h3></a></div>
-        <div class="col"><a class="icon-box card card-body h-100 border-0 shadow card-hover h-100 text-center"
-                            href="">
+        <div class="col"><a wire:navigate class="icon-box card card-body h-100 border-0 shadow card-hover h-100 text-center"
+                            href="{{ route('blogIndex',['cid'=>5]) }}">
                 <div class="icon-box-media bg-faded-primary text-primary rounded-circle mb-3 mx-auto"><i
                         class="fi-apartment"></i></div>
                 <h3 class="icon-box-title fs-base mb-0">تعمیرات </h3></a></div>
