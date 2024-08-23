55483fe2c71f9eca40e3b45fe0610abd8624a12f
Denason <mohammad.wartan@gmail.com>
2024-08-23 12:10:41 +0330

    optimize page load by remove assetes

--- resources/views/livewire/front/home.blade.php
+++ resources/views/livewire/front/home.blade.php
@@ -11,8 +11,6 @@
 
     @section('css')
 
-        <link rel="stylesheet" media="screen" href="{{ asset('assets/vendor/simplebar/dist/simplebar.min.css') }}"/>
-        <link rel="stylesheet" media="screen" href="{{ asset('assets/vendor/nouislider/dist/nouislider.min.css') }}"/>
         <link rel="stylesheet" media="screen" href="{{ asset('assets/vendor/tiny-slider/dist/tiny-slider.css') }}"/>
 
     @endsection
@@ -32,9 +30,9 @@
 @section('js')
             <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
             <script data-navigate-once src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-            <script data-navigate-onc src="{{ asset('assets/vendor/simplebar/dist/simplebar.min.js') }}"></script>
+
             <script data-navigate-onc src="{{ asset('assets/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js') }}"></script>
-            <script data-navigate-onc src="{{ asset('assets/vendor/nouislider/dist/nouislider.min.js') }}"></script>
+
             <script data-navigate-onc src="{{ asset('assets/vendor/tiny-slider/dist/min/tiny-slider.js') }}"></script>
 
             <script data-navigate-onc src="{{ asset('assets/js/theme.min.js') }}"></script>
