61f3a230869d8045f2d22256a826474c7c468584
Denason <mohammad.wartan@gmail.com>
2024-08-24 16:55:47 +0330

    expert page modify for mobile

--- resources/views/livewire/front/user/expert-inc/navigation.blade.php
+++ resources/views/livewire/front/user/expert-inc/navigation.blade.php
@@ -1,8 +1,8 @@
-<div class="container mt-3 mb-md-4 pt-5">
+<div class="container mt-5 mb-md-4 pt-5">
     <nav class="mb-3 pt-md-3" aria-label="breadcrumb">
-        <ol class="breadcrumb d-none d-md-block">
-            <li class="breadcrumb-item"><a wire:navigate href="{{ route('home') }}">خانه</a></li>
-            <li class="breadcrumb-item active" aria-current="page"> مشخصات کارشناس فنی</li>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item  d-none d-md-block"><a wire:navigate href="{{ route('home') }}">خانه</a></li>
+            <li class="breadcrumb-item active  d-none d-md-block" aria-current="page"> مشخصات کارشناس فنی</li>
         </ol>
     </nav>
 </div>
