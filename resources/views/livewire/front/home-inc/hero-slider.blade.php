abb58f57ee82e5b6100ade2a6ab0624acc3e4ecd
Denason <mohammad.wartan@gmail.com>
2024-08-27 12:06:41 +0330

    fix slider

--- resources/views/livewire/front/home-inc/hero-slider.blade.php
+++ resources/views/livewire/front/home-inc/hero-slider.blade.php
@@ -1,6 +1,7 @@
 @php
-    $slider = \App\Models\Slider::with('images')->whereCaption('hero-slider')->first() ?? 'No Slider found';
+    $slider = \App\Models\Slider::with('images')?->whereCaption('hero-slider')?->first();
 @endphp
+@if($slider->images->count() > 0)
 <div class="tns-carousel-wrapper tns-nav-outside tns-nav-outside-flush" dir="ltr">
     <div class="tns-carousel-inner"
          data-carousel-options='{
@@ -24,7 +25,7 @@
 
 
 
-        @if($slider && $slider->images)
+        @if($slider && $slider?->images)
             @foreach($slider->images as $image)
                 <div class="col mx-2 position-relative" style="height: 400px;">
                     <img src="{{ asset($image->file_path) }}" class="rounded" alt="" style="height: 100%; width: 100%; object-fit: cover;">
@@ -49,3 +50,4 @@
 
 </div>
 
+@endif
