b821257a39607bdacfb626d1ba694c0562ff3020
Denason <mohammad.wartan@gmail.com>
2024-08-30 13:14:04 +0330

    update scripts

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,46 +2,48 @@
 <html lang="{{ app()->getLocale() }}">
 
 <head>
-
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <meta charset="utf-8">
-        <title>{{ $title ?? '' }}</title>
-        <link rel="canonical" href="{{ url()->current() }}">
+    <title>{{ $title ?? '' }}</title>
+    <link rel="canonical" href="{{ url()->current() }}">
 
-        @yield('meta')
-        <!-- Viewport-->
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Favicon and Touch Icons-->
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favico.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favico.png') }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favico.png') }}">
-{{--        <link rel="manifest" href="site.webmanifest">--}}
-        <link rel="mask-icon" color="#5bbad5" href="{{ asset('favico.png') }}">
-        <meta name="msapplication-TileColor" content="#766df4">
-        <meta name="theme-color" content="#ffffff">
-        <script src="{{ asset('admin/assets/libs/jquery/jquery.min.js') }}"></script>
-    <script data-navigate-onc src="{{ asset('assets/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js') }}"></script>
+    @yield('meta')
 
-@yield('css')
-@stack('styles')
+    <!-- Favicon and Touch Icons-->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favico.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favico.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favico.png') }}">
+    <link rel="mask-icon" color="#5bbad5" href="{{ asset('favico.png') }}">
+    <meta name="msapplication-TileColor" content="#766df4">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Styles -->
+    @yield('css')
+    @stack('styles')
+    @livewireStyles
+
+    <!-- Core JS Files -->
+    <script src="{{ asset('admin/assets/libs/jquery/jquery.min.js') }}" defer></script>
+    <script src="{{ asset('assets/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js') }}" defer></script>
 </head>
 
 <body dir="rtl">
-@livewireScripts
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
 <div id="main-wrapper">
-
-{{ $slot }}
-
+    {{ $slot }}
 </div>
-<x-livewire-alert::scripts/>
 
-
-<script data-navigate-once src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+<!-- Scripts -->
 @yield('js')
-<script data-navigate-onc src="{{ asset('assets/js/theme.min.js') }}"></script>
+<script src="{{ asset('assets/js/theme.min.js') }}" defer></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
+<script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}" defer></script>
+
+
+@livewireScripts
+<x-livewire-alert::scripts/>
 </body>
 
 </html>
