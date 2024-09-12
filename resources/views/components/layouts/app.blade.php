fc63baad20ee8c7ed9009d30943f4f27bff67ad8
Denason <mohammad.wartan@gmail.com>
2024-09-12 19:56:17 +0330

    allot

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -39,11 +39,12 @@
     {{ $slot }}
 
 </div>
+<script data-navigate-onc src="{{ asset('assets/js/theme.min.js') }}"></script>
 <script data-navigate-once src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
 <x-livewire-alert::scripts/>
 
 
-<script data-navigate-onc src="{{ asset('assets/js/theme.min.js') }}"></script>
+
 
 @yield('js')
 
